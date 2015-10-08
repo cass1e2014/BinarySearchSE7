69a0eecf7283af2b499124c1ccfe637da25a9e24
cass1e2014 <caoyl767@hotmail.com>
2015-10-08 00:35:31 -0400

    add new work

---
+++ src/SearchA2DMatrixII.java
@@ -0,0 +1,58 @@
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * Integers in each row are sorted in ascending from left to right. Integers in
+ * each column are sorted in ascending from top to bottom. For example,
+ * 
+ * Consider the following matrix:
+ * 
+ * [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+  ]
+ * 
+ * Given target = 5, return true.
+ * Given target = 20, return false.
+ * 
+ * 从最右上角开始做。由于左边的元素一定不大于当前元素，而下面的元素一定不小于当前元素，因此每次比较时均可排除一列或者一行元素
+ * 复杂度要求——O(m+n) time and O(1) extra space
+ * @author cassie9082
+ * 
+ */
+public class SearchA2DMatrixII {
+
+	public static void main(String[] args) {
+		int[][] matrix = {{-5}};
+		System.out.println(searchMatrix(matrix, -5));
+	}
+
+	public static boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix[0] == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        
+        int row = matrix.length - 1;
+        int column = matrix[0].length - 1;
+        int index_row = 0;
+        int index_column = column;
+        int ele;
+        while(index_row < row + 1 && index_column + 1 > 0){
+            ele = matrix[index_row][index_column];
+            if(ele == target){
+                return true;
+            }
+            else if(target < ele){
+                index_column --;
+            }
+            else if(target > ele){
+                index_row ++;
+            }
+        }
+        return false;
+    }
+	
+}
